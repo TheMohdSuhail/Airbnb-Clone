4500bd378613e33d1a5eadc394977f2a1699f505
Mohd Suhail <msp215721@gmail.com>
2024-04-27 08:03:58 +0530

    Solve some error

--- init/index.js
+++ init/index.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://themsp:3gcFMvu95Q5BHO1w@cluster0.gnumkkz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 main()
   .then(() => {
@@ -17,9 +18,9 @@ async function main() {
 }
 
 const initDB = async () => {
-  await Listing.deleteMany({});
- initData.data =  initData.data.map((Obj)=>({...Obj,owner:"661e76f7971c3cac39b819e6"}));
-  await Listing.insertMany(initData.data);
+  // await Listing.deleteMany({});
+//  initData.data =  initData.data.map((Obj)=>({...Obj,owner:"661e76f7971c3cac39b819e6"}));
+  // await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
 
